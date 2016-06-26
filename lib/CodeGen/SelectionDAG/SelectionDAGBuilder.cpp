10492a10cf90629ade109205ece516c2983c7658
Juneyoung Lee <aqjune@gmail.com>
2016-06-26 16:01:06 +0900

    Add target lowering, add type checking, add InstCombineFreeze.cpp

--- lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
+++ lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
@@ -9356,5 +9356,6 @@ void SelectionDAGBuilder::visitSwitch(const SwitchInst &SI) {
 }
 
 void SelectionDAGBuilder::visitFreeze(const FreezeInst &I) {
-  assert(false && "SelectionDAGBuilder::visitFreeze Not Implemented");
+  SDValue N = getValue(I.getOperand(0));
+  setValue(&I, N);
 }
---
+++ lib/Transforms/InstCombine/InstCombineFreeze.cpp
@@ -0,0 +1,26 @@
+//===- InstCombineFreeze.cpp ----------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the visitFreeze function.
+//
+//===----------------------------------------------------------------------===//
+
+#include "InstCombineInternal.h"
+using namespace llvm;
+using namespace PatternMatch;
+
+#define DEBUG_TYPE "instcombine"
+
+Instruction *InstCombiner::visitFreeze(FreezeInst &FI) {
+  Value *Op0 = FI.getOperand(0);
+  if (FreezeInst *Op0_FI = dyn_cast<FreezeInst>(Op0))
+    return Op0_FI;
+
+  return nullptr;
+}
